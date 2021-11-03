8cd7d6eb720128ca076cc92cb71f2ea15f3523e2
Coroama Betuela Madalina <betuela.coroama@gmail.com>
2021-11-03 20:34:26 +0200

    Tema4

---
+++ Laborator_4/Randomizer.cs
@@ -0,0 +1,31 @@
+﻿using OpenTK;
+using System;
+using System.Drawing;
+
+namespace Laborator_4
+{
+    class Randomizer
+    {
+        private Random r;
+     
+
+        //constructor implicit
+        public Randomizer()
+        {
+            r = new Random();
+        }
+        //generarea culorii random
+        public Color GetRandomColor()
+        {
+            int genR = r.Next(0, 255);
+            int genG = r.Next(0, 255);
+            int genB = r.Next(0, 255);
+
+            Color col = Color.FromArgb(genR, genG, genB);
+
+            return col;
+        }
+
+        
+    }
+}
